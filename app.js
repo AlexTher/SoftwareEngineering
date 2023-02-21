4584501eb829f0345bba7a3fc4e8bc03693e0359
APTanner <atanner@trinity.edu>
2023-02-21 17:18:00 -0600

    created database

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
-
 const app = express();
+const mongoose = require('mongoose');
+require('dotenv/config');
 
 // Set view engine
 app.set('view engine', 'ejs');
@@ -10,6 +11,13 @@ var loginRouter = require('./routes/login');
 
 // Routes
 app.use('/', loginRouter);
+
+// Connect to database
+mongoose.set('strictQuery', false);
+mongoose.connect(
+    process.env.DB_CONNECTION_URL, 
+    () => console.log('Connected to database')
+);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, console.log(`Server on port ${PORT}`));
