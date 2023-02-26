a598868e87a7e87338cf7f17f5baa397956668cd
jimmyadkins <jimmyadkins@me.com>
2023-02-25 18:16:49 -0600

    Added login/auth stuff

---
+++ models/User.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose');
+
+const UserSchema = new mongoose.Schema({
+    name: { type: String, required: true },
+    ID: { type: Number, required: true },
+    email: { type: String, required: true, unique: true },
+    password: { type: String, required: true },
+    role: { type: String, enum: ['student', 'teacher', 'admin'], required: true },
+    data: { type: mongoose.Schema.Types.ObjectId, refPath: 'role' },
+})
+
+module.exports = mongoose.model('User', UserSchema);
