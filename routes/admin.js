a721f49e1773ba2e4c9f34f250fb0ee1f7b7faf7
Kinsey Neas <kinseyneas@Kinseys-MacBook-Air.local>
2023-03-26 15:30:17 -0500

    Added Student Pages.

--- routes/admin.js
+++ routes/admin.js
@@ -4,49 +4,49 @@ const { roles, restrictAccess } = require('../functions/authentication/auth');
 const Schedule = require('../models/Schedule');
 const Subject = require('../models/Subject');
 
-router.get('/admin/dashboard', /*restrictAccess(roles.ADMIN),*/ (req, res) => {
-    res.render('/users/admin/dashboard', { user: req.session.user });
+router.get('/dashboard', /*restrictAccess(roles.ADMIN),*/ (req, res) => {
+    res.render('admin/dashboard', { user: req.session.user });
 })
 
 
 //These are the routes for the admin pages
-router.get('/admin/create-user', /*restrictAccess(roles.ADMIN),*/ (req, res) => {
-    res.render('/users/admin/create-user');
+router.get('/create-user', /*restrictAccess(roles.ADMIN),*/ (req, res) => {
+    res.render('admin/create-user');
 });
-router.get('/admin/create-class', /*restrictAccess(roles.ADMIN),*/ async (req, res) => {
-    try {
+router.get('/create-class', /*restrictAccess(roles.ADMIN),*/ async (req, res) => {
+    try { 
         // Fetch all schedules from the database
         const schedules = await Schedule.find();
         const departments = await Subject.distinct('department');
         //const subjects = await Subject.find({ department: req.query.department });
 
         // Render the class creation form template and pass the schedules variable
-        res.render('/users/admin/create-class', { schedules, departments });
+        res.render('admin/create-class', { schedules, departments });
     } catch (err) {
         console.error(err);
         res.status(500).send('Server error');
     }
 });
-router.get('/admin/create-subject', /*restrictAccess(roles.ADMIN),*/ async (req, res) => {
+router.get('/create-subject', /*restrictAccess(roles.ADMIN),*/ async (req, res) => {
     try {
         // Fetch all schedules from the database
         const departments = await Subject.distinct('department');
 
         // Render the class creation form template and pass the schedules variable
-        res.render('/users/admin/create-subject', { departments });
+        res.render('admin/create-subject', { departments });
     } catch (err) {
         console.error(err);
         res.status(500).send('Server error');
     }
 });
-router.get('/admin/calendar', /*restrictAccess(roles.ADMIN),*/ (req, res) => {
-    res.render('/users/admin/calendar',{text: 'Hey'});
+router.get('/calendar', /*restrictAccess(roles.ADMIN),*/ (req, res) => {
+    res.render('admin/calendar',{text: 'Hey'});
 });
-router.get('/admin/classes', /*restrictAccess(roles.ADMIN),*/ (req, res) => {
-    res.render('/users/admin/classes');
+router.get('/classes', /*restrictAccess(roles.ADMIN),*/ (req, res) => {
+    res.render('admin/classes');
 });
-router.get('/admin/users', /*restrictAccess(roles.ADMIN),*/ (req, res) => {
-    res.render('/users/admin/users');
+router.get('/users', /*restrictAccess(roles.ADMIN),*/ (req, res) => {
+    res.render('admin/users');
 });
 
 module.exports = router;
