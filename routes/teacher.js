e0ce6446930792254bd0e21e3719a5cde2cb1697
SStepanik <seanstepanik@gmail.com>
2023-03-28 19:56:21 -0500

    added teacher web pages

--- routes/teacher.js
+++ routes/teacher.js
@@ -6,6 +6,26 @@ router.get('/dashboard', restrictAccess(roles.TEACHER), (req, res) => {
     res.render('teacher/dashboard', { user: req.session.user });
 })
 
-//other routes go here
+//These are the routes for the admin pages
+
+router.get('/calendar', restrictAccess(roles.TEACHER), (req, res) => {
+    res.render('teacher/calendar',{text: 'Hey'});
+});
+
+router.get('/classes', restrictAccess(roles.TEACHER), (req, res) => {
+    res.render('teacher/classes');
+});
+
+router.get('/users', restrictAccess(roles.TEACHER), (req, res) => {
+    res.render('teacher/users');
+});
+
+router.get('/registration', restrictAccess(roles.TEACHER), (req, res) => {
+    res.render('teacher/registration');
+});
+
+router.get('/search', restrictAccess(roles.TEACHER), (req, res) => {
+    res.render('teacher/search');
+});
 
 module.exports = router;
