8aedf61e10093650d07daa13cb709dbbd9ffdcec
SStepanik <seanstepanik@gmail.com>
2023-03-30 21:15:51 -0500

    added class search capability

--- routes/teacher.js
+++ routes/teacher.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const router = express.Router();
 const { roles, restrictAccess } = require('../functions/authentication/auth');
+const Schedule = require('../models/Schedule');
+const Subject = require('../models/Subject');
 
 router.get('/dashboard', restrictAccess(roles.TEACHER), (req, res) => {
     res.render('teacher/dashboard', { user: req.session.user });
@@ -12,8 +14,20 @@ router.get('/calendar', restrictAccess(roles.TEACHER), (req, res) => {
     res.render('teacher/calendar',{text: 'Hey'});
 });
 
-router.get('/classes', restrictAccess(roles.TEACHER), (req, res) => {
-    res.render('teacher/classes');
+router.get('/classes', restrictAccess(roles.TEACHER), async (req, res) => {
+    try {
+        // Get stuff from database
+        const schedules = await Schedule.find();
+        const subjects = await Subject.find();
+        const departments = await Subject.distinct('department');
+        const pathways = await Subject.distinct('pathways');
+        const credits = await Subject.distinct('credits')
+        res.render('teacher/classes', { schedules, subjects, departments, pathways, credits});
+        
+    } catch (err) {
+        console.error(err);
+        res.status(500).send('Server error');
+    }
 });
 
 router.get('/registration', restrictAccess(roles.TEACHER), (req, res) => {
